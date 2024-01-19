72776fbdd332381f26d05c1e1bb8ebde795aec36
yeinernaranjo <yeinernaranjo@gmail.com>
2024-01-19 17:55:21 -0500

    Se finalizó la pagina principal, falta hacer responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				custom: 'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px',
+			  },
+		},
 	},
 	plugins: [],
 }
