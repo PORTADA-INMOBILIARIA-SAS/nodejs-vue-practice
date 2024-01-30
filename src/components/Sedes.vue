61e11d96e9b5d5e2d209395f694bc70af1e596f2
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-01-30 09:33:25 -0500

    feat: husky and prettier pa' joder
    test: example
    
    test: le va a tocar vivir con eso
    
    test: me decepciona la programacion
    
    test: me decepciona la programacion 2 Electric Boogaloo
    
    test: otro pinche commit que no tiene nah

--- src/components/Sedes.vue
+++ src/components/Sedes.vue
@@ -1,63 +1,105 @@
 <template>
   <div class="grid grid-cols-2 mx-4 text-sm md:text-lg">
     <div class="col-start-1 col-end-2">
+      <!-- Esto esta mal indentado -->
       <div class="w-[100%] bg-gray-900 text-white uppercase px-2 py-1">
         <span class="tracking-widest font-bold">ANTIOQUIA</span>
       </div>
-      <div class="min-w-fit w-[95%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[97%]" v-on:click="sedeSeleccionada = 'laureles'">
+      <div
+        class="min-w-fit w-[95%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[97%]"
+        v-on:click="sedeSeleccionada = 'laureles'"
+      >
         <span class="tracking-widest">Laureles</span>
       </div>
-      <div class="min-w-fit w-[90%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[92%]" v-on:click="sedeSeleccionada = 'bello'">
+      <div
+        class="min-w-fit w-[90%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[92%]"
+        v-on:click="sedeSeleccionada = 'bello'"
+      >
         <span class="tracking-widest">Bello</span>
       </div>
-      <div class="min-w-fit w-[85%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[87%]" v-on:click="sedeSeleccionada = 'colores'">
+      <div
+        class="min-w-fit w-[85%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[87%]"
+        v-on:click="sedeSeleccionada = 'colores'"
+      >
         <span class="tracking-widest">Los Colores</span>
       </div>
-      <div class="min-w-fit w-[80%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[82%]" v-on:click="sedeSeleccionada = 'poblado'">
+      <div
+        class="min-w-fit w-[80%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[82%]"
+        v-on:click="sedeSeleccionada = 'poblado'"
+      >
         <span class="tracking-widest">El Poblado</span>
       </div>
-      <div class="min-w-fit w-[75%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[77%]" v-on:click="sedeSeleccionada = 'envigado'">
+      <div
+        class="min-w-fit w-[75%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[77%]"
+        v-on:click="sedeSeleccionada = 'envigado'"
+      >
         <span class="tracking-widest">Envigado</span>
       </div>
-      <div class="min-w-fit w-[70%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[72%]" v-on:click="sedeSeleccionada = 'sabaneta'">
+      <div
+        class="min-w-fit w-[70%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[72%]"
+        v-on:click="sedeSeleccionada = 'sabaneta'"
+      >
         <span class="tracking-widest">Sabaneta</span>
       </div>
-      <div class="min-w-fit w-[65%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[67%]" v-on:click="sedeSeleccionada = 'estrella'">
+      <div
+        class="min-w-fit w-[65%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[67%]"
+        v-on:click="sedeSeleccionada = 'estrella'"
+      >
         <span class="tracking-widest">La Estrella</span>
       </div>
-      <div class="min-w-fit w-[60%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[62%]" v-on:click="sedeSeleccionada = 'rionegro'">
+      <div
+        class="min-w-fit w-[60%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[62%]"
+        v-on:click="sedeSeleccionada = 'rionegro'"
+      >
         <span class="tracking-widest">Rionegro</span>
       </div>
-      <div class="min-w-fit w-[55%] bg-gray-900 text-white uppercase px-2 py-1 mt-1">
+      <div
+        class="min-w-fit w-[55%] bg-gray-900 text-white uppercase px-2 py-1 mt-1"
+      >
         <span class="tracking-widest font-bold">Cundinamarca</span>
       </div>
-      <div class="min-w-fit w-[65%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[67%]" v-on:click="sedeSeleccionada = 'bogota'">
+      <div
+        class="min-w-fit w-[65%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[67%]"
+        v-on:click="sedeSeleccionada = 'bogota'"
+      >
         <span class="tracking-widest">Bogotá</span>
       </div>
-      <div class="min-w-fit w-[70%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[72%]" v-on:click="sedeSeleccionada = 'mosquera'">
+      <div
+        class="min-w-fit w-[70%] bg-gradient-to-r from-red-600 to-red-800 text-white uppercase px-2 py-1 mt-1 cursor-pointer transition-all duration-500 hover:shadow-custom hover:bg-red-800 hover:w-[72%]"
+        v-on:click="sedeSeleccionada = 'mosquera'"
+      >
         <span class="tracking-widest">Mosquera</span>
       </div>
     </div>
     <div class="col-start-2 col-end-3 text-right">
       <transition name="fade" mode="out-in">
-        <div class="flex flex-col h-[100%] justify-center items-end" v-bind:key="sedeSeleccionada">
-          <div class="bg-gradient-to-r from-red-600 to-red-800 text-white px-2 py-1 w-auto">
-              <span class="text-xl font-bold">
-                  {{sedeSeleccionada == 'laureles' ? 'SEDE PRINCIPAL' : 'SEDE'}}
-              </span>
+        <div
+          class="flex flex-col h-[100%] justify-center items-end"
+          v-bind:key="sedeSeleccionada"
+        >
+          <div
+            class="bg-gradient-to-r from-red-600 to-red-800 text-white px-2 py-1 w-auto"
+          >
+            <span class="text-xl font-bold">
+              {{ sedeSeleccionada == "laureles" ? "SEDE PRINCIPAL" : "SEDE" }}
+            </span>
           </div>
           <div class="mt-2">
-            <span class="text-4xl md:text-7xl font-bold uppercase">{{infoSedes[sedeSeleccionada].nombre}}</span>
+            <span class="text-4xl md:text-7xl font-bold uppercase">{{
+              infoSedes[sedeSeleccionada].nombre
+            }}</span>
           </div>
           <div class="mt-5 text-lg">
-          <span>{{infoSedes[sedeSeleccionada].telefono}}</span>
-            <br>
-            <span>{{infoSedes[sedeSeleccionada].direccion}}</span>
-            <br>
-            <span class="border-b-2 hover:border-b-red-600 cursor-pointer">Ver Asesores</span>
+            <span>{{ infoSedes[sedeSeleccionada].telefono }}</span>
+            <br />
+            <span>{{ infoSedes[sedeSeleccionada].direccion }}</span>
+            <br />
+            <span class="border-b-2 hover:border-b-red-600 cursor-pointer"
+              >Ver Asesores</span
+            >
           </div>
           <div>
-            <img src="/img/logo/logo-horizontal-portada.webp?url" alt="">
+            <img src="/img/logo/logo-horizontal-portada.webp?url" alt="" />
           </div>
         </div>
       </transition>
@@ -69,75 +111,77 @@
 export default {
   data() {
     return {
-      sedeSeleccionada: 'laureles',
+      sedeSeleccionada: "laureles",
       infoSedes: {
         laureles: {
-          nombre: 'Laureles',
-          direccion: 'Carrera 81 # 35 - 96',
-          telefono: '(604) 4487771 Opción 1, 1, 1'
+          nombre: "Laureles",
+          direccion: "Carrera 81 # 35 - 96",
+          telefono: "(604) 4487771 Opción 1, 1, 1",
         },
         bello: {
-          nombre: 'Bello',
-          direccion: 'Calle 26B # 49 - 104 Of 206',
-          telefono: '(604) 4487771 Opción 1, 1, 1'
+          nombre: "Bello",
+          direccion: "Calle 26B # 49 - 104 Of 206",
+          telefono: "(604) 4487771 Opción 1, 1, 1",
         },
         colores: {
-          nombre: 'Los Colores',
-          direccion: 'Calle 53 # 73 - 31',
-          telefono: '(604) 4487771 Opción 1, 1, 1'
+          nombre: "Los Colores",
+          direccion: "Calle 53 # 73 - 31",
+          telefono: "(604) 4487771 Opción 1, 1, 1",
         },
         poblado: {
-          nombre: 'El Poblado',
-          direccion: 'Calle 10# 43D - 68 Local 2',
-          telefono: '(604) 4487771 Opción 1, 1, 1'
+          nombre: "El Poblado",
+          direccion: "Calle 10# 43D - 68 Local 2",
+          telefono: "(604) 4487771 Opción 1, 1, 1",
         },
         envigado: {
-          nombre: 'Envigado',
-          direccion: 'Carrera 43A # 35 Sur - 15',
-          telefono: '(604) 4487771 Opción 1, 1, 1'
+          nombre: "Envigado",
+          direccion: "Carrera 43A # 35 Sur - 15",
+          telefono: "(604) 4487771 Opción 1, 1, 1",
         },
         sabaneta: {
-          nombre: 'Sabaneta',
-          direccion: 'Calle 70 Sur - 43A - 09',
-          telefono: '(604) 4487771 Opción 1, 1, 1'
+          nombre: "Sabaneta",
+          direccion: "Calle 70 Sur - 43A - 09",
+          telefono: "(604) 4487771 Opción 1, 1, 1",
         },
         estrella: {
-          nombre: 'La Estrella',
-          direccion: 'Calle 75AA Sur # 53 - 198',
-          telefono: '(604) 4487771 Opción 1, 1, 1'
+          nombre: "La Estrella",
+          direccion: "Calle 75AA Sur # 53 - 198",
+          telefono: "(604) 4487771 Opción 1, 1, 1",
         },
         rionegro: {
-          nombre: 'Rionegro',
-          direccion: 'Calle 47 # 62 - 18',
-          telefono: '(604) 4487771 Opción 1, 1, 1'
+          nombre: "Rionegro",
+          direccion: "Calle 47 # 62 - 18",
+          telefono: "(604) 4487771 Opción 1, 1, 1",
         },
         bogota: {
-          nombre: 'Fontibon',
-          direccion: 'Calle 18 # 77 - 51 Local 212',
-          telefono: '3009127843 Opción 1, 1, 2'
+          nombre: "Fontibon",
+          direccion: "Calle 18 # 77 - 51 Local 212",
+          telefono: "3009127843 Opción 1, 1, 2",
         },
         mosquera: {
-          nombre: 'Mosquera',
-          direccion: 'Carrera 3A # 17 Sur - 96 Local 114',
-          telefono: '3009127843 Opción 1, 1, 2'
-        }
-      }
-    };
+          nombre: "Mosquera",
+          direccion: "Carrera 3A # 17 Sur - 96 Local 114",
+          telefono: "3009127843 Opción 1, 1, 2",
+        },
+      },
+    }
   },
   methods: {
     handleSedeClick(sede) {
-      this.sedeSeleccionada = sede;
-    }
-  }
-};
+      this.sedeSeleccionada = sede
+    },
+  },
+}
 </script>
 
 <style>
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .3s;
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
 
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
