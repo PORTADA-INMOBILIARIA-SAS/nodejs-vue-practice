2bf3084ec334d157d8cf88f92427163a11074918
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-02-10 10:49:47 -0500

    feat: azure dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import vue from "@astrojs/vue"
 
-import netlify from "@astrojs/netlify/functions"
+// import netlify from "@astrojs/netlify/functions"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: "hybrid",
-  adapter: netlify(),
+  //   adapter: netlify(),
 })
