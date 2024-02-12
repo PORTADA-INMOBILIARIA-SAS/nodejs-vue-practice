0ca1a2cd74c8eae4380d2cef8f74d575a30e27ee
yeinernaranjo <yeinernaranjo@gmail.com>
2024-02-12 12:07:28 -0500

    feat: deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import vue from "@astrojs/vue"
-
 import netlify from "@astrojs/netlify"
+import deno from "@astrojs/deno"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  output: "hybrid",
-  adapter: netlify(),
+  output: "server",
+  adapter: deno(),
 })
