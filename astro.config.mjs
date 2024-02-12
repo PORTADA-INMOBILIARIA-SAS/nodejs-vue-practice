3066c4f1d33228d3db51a8cabcd74fb726405c0e
yeinernaranjo <yeinernaranjo@gmail.com>
2024-02-12 12:43:18 -0500

    feat: hybrid to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import vue from "@astrojs/vue"
 
-// import netlify from "@astrojs/netlify";
-import fastify from "@matthewp/astro-fastify"
+import netlify from "@astrojs/netlify"
+// import fastify from "@matthewp/astro-fastify"
 // import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: "server",
-  adapter: fastify({
-    port: 8085,
-  }),
+  adapter: netlify(),
 })
