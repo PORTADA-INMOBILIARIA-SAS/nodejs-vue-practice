a4b141b8ae14e28e8de24c19525617271303ed20
yeinernaranjo <yeinernaranjo@gmail.com>
2024-02-12 10:59:02 -0500

    fix: intento 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import vue from "@astrojs/vue"
 
-import netlify from "@astrojs/netlify/functions"
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
