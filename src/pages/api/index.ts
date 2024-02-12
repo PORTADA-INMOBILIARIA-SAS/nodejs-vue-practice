3d1a27d3d2901ad8d6db9b56671b899abe07270b
yeinernaranjo <yeinernaranjo@gmail.com>
2024-02-12 12:49:19 -0500

    fix: elimino archivo innecesario

--- src/pages/api/index.ts
+++
@@ -1,9 +0,0 @@
-import type { DefineFastifyRoutes } from "@matthewp/astro-fastify"
-
-const defineRoutes: DefineFastifyRoutes = (fastify) => {
-  fastify.get("/api/todos", function (_, reply) {
-    reply.send("Hello World")
-  })
-}
-
-export default defineRoutes
