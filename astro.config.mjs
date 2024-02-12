6d8b4e8603daf863432ad79f8ff07c0f4c77999e
yeinernaranjo <yeinernaranjo@gmail.com>
2024-02-12 12:12:22 -0500

    fix: p

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import vue from "@astrojs/vue"
 import netlify from "@astrojs/netlify"
-import deno from "@astrojs/deno"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  output: "server",
-  adapter: deno(),
+  output: "hybrid",
+  adapter: netlify(),
 })
