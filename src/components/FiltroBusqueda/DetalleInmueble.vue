047775d2ad4f65bb0ea67c20c030e735efbbab91
yeinernaranjo <yeinernaranjo@gmail.com>
2024-02-20 16:15:22 -0500

    feat: changed arrow svg

--- src/components/FiltroBusqueda/DetalleInmueble.vue
+++ src/components/FiltroBusqueda/DetalleInmueble.vue
@@ -39,12 +39,12 @@
           class="relative flex justify-center max-h-screen backdrop-blur-3xl"
         >
           <button
-            class="absolute left-0 top-[50%] text-white ml-5 bg-[--primary-color] rounded-full"
+            class="absolute left-0 top-[50%] text-white ml-5 rounded-full bg-gradient-to-r from-[--primary-color] to-red-800"
             @click="previousImage"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-circle-arrow-left-filled"
+              class="icon icon-tabler icon-tabler-circle-arrow-left"
               width="35"
               height="35"
               viewBox="0 0 24 24"
@@ -55,20 +55,19 @@
               stroke-linejoin="round"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path
-                d="M12 2a10 10 0 0 1 .324 19.995l-.324 .005l-.324 -.005a10 10 0 0 1 .324 -19.995zm.707 5.293a1 1 0 0 0 -1.414 0l-4 4a1.048 1.048 0 0 0 -.083 .094l-.064 .092l-.052 .098l-.044 .11l-.03 .112l-.017 .126l-.003 .075l.004 .09l.007 .058l.025 .118l.035 .105l.054 .113l.043 .07l.071 .095l.054 .058l4 4l.094 .083a1 1 0 0 0 1.32 -1.497l-2.292 -2.293h5.585l.117 -.007a1 1 0 0 0 -.117 -1.993h-5.586l2.293 -2.293l.083 -.094a1 1 0 0 0 -.083 -1.32z"
-                stroke-width="0"
-                fill="currentColor"
-              />
+              <path d="M12 21a9 9 0 1 0 0 -18a9 9 0 0 0 0 18" />
+              <path d="M8 12l4 4" />
+              <path d="M8 12h8" />
+              <path d="M12 8l-4 4" />
             </svg>
           </button>
           <button
-            class="absolute right-0 top-[50%] text-white mr-5 bg-[--primary-color] rounded-full"
+            class="absolute right-0 top-[50%] text-white mr-5 rounded-full bg-gradient-to-r from-[--primary-color] to-red-800"
             @click="nextImage"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-circle-arrow-right-filled"
+              class="icon icon-tabler icon-tabler-circle-arrow-right"
               width="35"
               height="35"
               viewBox="0 0 24 24"
@@ -79,11 +78,10 @@
               stroke-linejoin="round"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path
-                d="M12 2l.324 .005a10 10 0 1 1 -.648 0l.324 -.005zm.613 5.21a1 1 0 0 0 -1.32 1.497l2.291 2.293h-5.584l-.117 .007a1 1 0 0 0 .117 1.993h5.584l-2.291 2.293l-.083 .094a1 1 0 0 0 1.497 1.32l4 -4l.073 -.082l.064 -.089l.062 -.113l.044 -.11l.03 -.112l.017 -.126l.003 -.075l-.007 -.118l-.029 -.148l-.035 -.105l-.054 -.113l-.071 -.111a1.008 1.008 0 0 0 -.097 -.112l-4 -4z"
-                stroke-width="0"
-                fill="currentColor"
-              />
+              <path d="M12 3a9 9 0 1 0 0 18a9 9 0 0 0 0 -18" />
+              <path d="M16 12l-4 -4" />
+              <path d="M16 12h-8" />
+              <path d="M12 16l4 -4" />
             </svg>
           </button>
           <img
