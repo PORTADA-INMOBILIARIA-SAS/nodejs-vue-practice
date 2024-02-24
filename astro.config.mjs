26ed6adc95328044f38e1cd54ff18a6e9af59c9f
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-02-24 08:18:19 -0500

    feat: dep for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import vue from "@astrojs/vue"
+// import netlify from "@astrojs/netlify";
 
-import netlify from "@astrojs/netlify"
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 })
